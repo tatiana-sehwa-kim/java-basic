185f4c80afb5fffbc9a19e970a0efcb373cffd83
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-03 21:43:20 +0900

    객체 지향 프로그래밍

---
+++ src/oop1/ValueData.java
@@ -0,0 +1,15 @@
+package oop1;
+
+public class ValueData {
+
+    int value;
+
+    void add() {
+        value++;
+        System.out.println("숫자 증가 value=" + value);
+    }
+}
+
+//클래스는 속성(데이터, 멤버 변수)과 기능(메서드)을 정의할 수 있다.
+//객체는 자신의 메서드를 통해 자신의 멤버 변수에 접근할 수 있다.
+//객체의 메서드 내부에서 접근하는 멤버 변수는 객체 자신의 멤버 변수이다.
