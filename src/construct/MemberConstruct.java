319039c912739ede3168538def41518cfa2fd265
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-04 22:21:06 +0900

    생성자 ○

--- src/construct/MemberConstruct.java
+++ src/construct/MemberConstruct.java
@@ -12,4 +12,16 @@ public class MemberConstruct {
         this.age = age;
         this.grade = grade;
     }
+
+    // 파라미터를 다르게 해서 다른 생성자도 만들수 있다. 이걸 "오버로딩"이라구.
+    // this()는 생성자 코드의 첫줄에만 작성할 수 있다.
+
+    MemberConstruct(String name, int age) {
+
+        this(name, age, 50); // 변경. 이렇게 자기자신의 생성자를 또 호출.
+
+//        this.name = name;
+//        this.age = age;
+//        this.grade = 50; //이름과 나이만 잇으면 성적은 50점이상으로시작한다 라고 할때.
+    }
 }
