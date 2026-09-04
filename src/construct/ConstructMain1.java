822d2be50c7366ed74d5f2ba9dc05b561156d248
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-04 21:57:55 +0900

    생성자 ○

--- src/construct/ConstructMain1.java
+++ src/construct/ConstructMain1.java
@@ -2,6 +2,13 @@ package construct;
 
 public class ConstructMain1 {
     public static void main(String[] args) {
-        MemberConstruct member1 = new MemberConstruct("user1",16,90); //객체를 생성해라 = 그 객체 이렇게 생성해라 란뜻
+        MemberConstruct member1 = new MemberConstruct("user1",15,90); //생성자 호출. 객체를 생성해라 = 그 객체 이렇게 생성해라 란뜻
+        MemberConstruct member2 = new MemberConstruct("user2",16,80);
+
+        MemberConstruct[] members = {member1, member2};
+
+        for (MemberConstruct s : members) {
+            System.out.println("이름: " + s.name + " 나이: " + s.age + " 성적: " + s.grade);
+        }
     }
 }
