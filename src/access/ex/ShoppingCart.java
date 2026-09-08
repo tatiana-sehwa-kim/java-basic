6deb8ba48b8b4bd8266fcde8db79cd5f44dfdb8a
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-09 00:11:29 +0900

    접근 제어자 예제- ShoppingCart/ Item

--- src/access/ex/ShoppingCart.java
+++ src/access/ex/ShoppingCart.java
@@ -19,9 +19,22 @@ public class ShoppingCart {
         System.out.println("장바구니 상품 출력");
 
         for (int i = 0; i < itemCount; i++) {
+//            System.out.println("상품명: " + items[i].getName() + ", 합계: " + items[i].getTotalPrice());
+//            total += items[i].getTotalPrice();    내가푼것. 이것도 맞긴한데 가독성 더 좋은 정답으로 써둠.
+
+            Item item = items[i];
             System.out.println("상품명: " + items[i].getName() + ", 합계: " + items[i].getTotalPrice());
-            total += items[i].getTotalPrice();
+
         }
         System.out.println("전체 가격 합: " + total);
     }
+
+//    private int calculateTotalPrice() {
+//        int totalPrice = 0;
+//        for (int i = 0; i < itemCount; i++) {
+//            Item item = items[i];
+//            totalPrice += item.getTotalPrice();
+//        }
+//        return totalPrice;
+//    }                         아예 이렇게 메서드로 빼는방법도 있다.
 }
