d0590d38c7119f856ac8c037791325b62dcdb3c2
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-15 22:08:42 +0900

    다형성2- 오버라이딩으로 프린트하기 (Animal Sound)

---
+++ src/poly/ex2/Animal.java
@@ -0,0 +1,8 @@
+package poly.ex2;
+
+public class Animal {
+
+    public void sound() {   // 오버라이딩 목적
+        System.out.println("동물 울음 소리");
+    }
+}
---
+++ src/poly/ex3/Animal.java
@@ -0,0 +1,8 @@
+package poly.ex3;
+
+public class Animal {
+
+    public void sound() {
+        System.out.println("울음소리");
+    }
+}
