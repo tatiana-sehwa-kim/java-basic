2a6d2c7b828d0085ee8279e6bb95e887d7779c8a
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-16 20:56:15 +0900

    다형성2- 오버라이딩으로 프린트하기

--- src/poly/ex3/Animal.java
+++
@@ -1,8 +0,0 @@
-package poly.ex3;
-
-public class Animal {
-
-    public void sound() {
-        System.out.println("울음소리");
-    }
-}
--- src/poly/ex3/Cat.java
+++
@@ -1,9 +0,0 @@
-package poly.ex3;
-
-public class Cat extends Animal{
-
-    @Override
-    public void sound() {
-        System.out.println("야옹");
-    }
-}
